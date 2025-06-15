b189acbaa1f04b65b26c90d3aa8637cb361a46b9
md-zeon <zeonrahaman5870@gmail.com>
2025-06-15 16:26:55 +0600

    Application and job api added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 const dotenv = require("dotenv");
+var jwt = require('jsonwebtoken');
 dotenv.config();
 
 // Middleware
@@ -30,11 +31,42 @@ async function run() {
 		const jobsCollection = careerCodeDB.collection("jobs");
 		const applicationsCollection = careerCodeDB.collection("applications");
 
+	
+
 		// Get all jobs
 		app.get("/jobs", async (req, res) => {
-			const cursor = jobsCollection.find();
+			const email = req.query.email;
+			let query = {};
+			if (email) {
+				query.hr_email = email;
+			}
+
+			const cursor = jobsCollection.find(query);
 			const result = await cursor.toArray();
 			res.send(result);
+		});
+
+		// could be done but should not be done
+		// app.get("/jobsByEmailAddress", async (req, res) => {
+		// 	const email = req.query.email;
+		// 	const query = { hr_email: email };
+		// 	const cursor = jobsCollection.find(query);
+		// 	const result = await cursor.toArray();
+		// 	res.send(result);
+		// });
+
+		app.get("/jobs/applications", async (req, res) => {
+			const email = req.query.email;
+			const query = { hr_email: email };
+			const jobs = await jobsCollection.find(query).toArray();
+
+			// should use aggregate to have optimum data fetching
+			for (const job of jobs) {
+				const applicationQuery = { jobId: job._id.toString() };
+				const application_count = await applicationsCollection.countDocuments(applicationQuery);
+				job.application_count = application_count;
+			}
+			res.send(jobs);
 		});
 
 		// Get a single job by ID
@@ -45,6 +77,13 @@ async function run() {
 			res.send(result);
 		});
 
+		app.post("/jobs", async (req, res) => {
+			const newJob = req.body;
+			console.log(newJob);
+			const result = await jobsCollection.insertOne(newJob);
+			res.send(result);
+		});
+
 		// Application related API
 		// Get all applications
 		// app.get("/applications", async (req, res) => {
@@ -52,6 +91,13 @@ async function run() {
 		// 	const result = await cursor.toArray();
 		// 	res.send(result);
 		// });
+
+		app.get("/applications/job/:job_id", async (req, res) => {
+			const job_id = req.params.job_id;
+			const query = { jobId: job_id };
+			const result = await applicationsCollection.find(query).toArray();
+			res.send(result);
+		});
 
 		// Get applications by email
 		app.get("/applications", async (req, res) => {
@@ -87,6 +133,18 @@ async function run() {
 			const id = req.params.id;
 			const query = { _id: new ObjectId(id) };
 			const result = await applicationsCollection.deleteOne(query);
+			res.send(result);
+		});
+
+		app.patch("/applications/:id", async (req, res) => {
+			const id = req.params.id;
+			const filter = { _id: new ObjectId(id) };
+			const updatedDoc = {
+				$set: {
+					status: req.body.status,
+				},
+			};
+			const result = await applicationsCollection.updateOne(filter, updatedDoc);
 			res.send(result);
 		});
 
